811be8faa299fe3a1695726b22efcd16bd7ca2df
Ada Shaw <ayshaw@g.harvard.edu>
2019-07-01 17:21:52 -0400

    the local repo was changed, I added the histogram plotting to the pipeline

--- rCode/plotting_histograms.R
+++ rCode/plotting_histograms.R
@@ -1,12 +1,17 @@
 rm(list=ls())
 library('DCAforR')
-setwd('/Users/adashaw/Dropbox (Harvard University)/Debbie-Ada/repeatsProject/')
-pfam = 'PF00023'
-source('~/Dropbox (Harvard University)/Debbie-Ada/repeatsProject/rCode/weight.idREP.R')
-file = paste0(pfam,'/pyOutput/',pfam,'_2reps_plm_input.txt')
-png(paste0(pfam,'/rOutput/',pfam,'_histogram.png'),height=600,width=600)
+### load inputs
+options(echo=TRUE) # To see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+working_dir = args[1]
+setwd(working_dir)
+pfam = args[2]
+source('/rCode/weight.idREP.R')
+file = paste0('/pyOutput/',pfam,'_2reps_plm_input.txt')
+png(paste0('/rOutput/',pfam,'_histogram.png'),height=600,width=600)
 msa<-readAlignment(file)
 msa <-msa2num(msa)
-wid <-weight.idREP_ada(msa,paste0(pfam,'/rOutput/',pfam,'_histogram.png'))
+wid <-weight.idREP_ada(msa,paste0('/rOutput/',pfam,'_histogram.png'))
 dev.off()
 
