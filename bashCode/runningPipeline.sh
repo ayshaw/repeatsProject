ff91324a9fd90d387fcf5b807c5b538a5ede3797
Shaw <as974@login01.o2.rc.hms.harvard.edu>
2019-07-01 20:53:21 -0400

    updated pipeline thing

--- bashCode/runningPipeline.sh
+++ bashCode/runningPipeline.sh
@@ -8,8 +8,8 @@ echo "$PFAM_DIR"
 echo $PFAM_DIR
 echo  ${PFAM_DIR}/${PFAM}_cor_submit.sh 
 Rscript ../rCode/rRepeatsPipeline.R $PFAM_DIR $PFAM
-python ../pyCode/pyRepeatsPipeline.R $PFAM_DIR $PFAM
-#Rscript ../rCode/plotting_histograms.R "'$PFAM_DIR'" "'$PFAM'"
-#python ../pyCode/python generating_bash_scripts.py "$PFAM_DIR" "$PFAM"
-#sbatch ${PFAM_DIR}/${PFAM}_cor_submit.sh 
+python ../pyCode/pyRepeatsPipeline.py $PFAM_DIR $PFAM
+Rscript ../rCode/plotting_histograms.R "'$PFAM_DIR'" "'$PFAM'"
+python ../pyCode/python generating_bash_scripts.py "$PFAM_DIR" "$PFAM"
+sbatch ${PFAM_DIR}/${PFAM}_cor_submit.sh 
 
