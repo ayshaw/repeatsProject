f91025094fd65356115f55d9125b5c0bca7f585d
Shaw <as974@compute-a-16-160.o2.rc.hms.harvard.edu>
2019-07-10 16:42:53 -0400

    changed splicing thing

--- bashCode/runningPipeline.sh
+++ bashCode/runningPipeline.sh
@@ -6,10 +6,9 @@ source activate evcouplings_develop
 #echo "'$PFAM_DIR'"
 #echo "$PFAM_DIR"
 #echo $PFAM_DIR
-#echo  ${PFAM_DIR}/${PFAM}_cor_submit.sh 
-#Rscript ../rCode/rRepeatsPipeline.R $PFAM_DIR $PFAM
-#python ../pyCode/pyRepeatsPipeline.py $PFAM_DIR $PFAM
+echo  ${PFAM_DIR}/${PFAM}_cor_submit.sh
+Rscript ../rCode/rRepeatsPipeline.R $PFAM_DIR $PFAM
+python ../pyCode/pyRepeatsPipeline.py $PFAM_DIR $PFAM
 Rscript ../rCode/plotting_histograms.R $PFAM_DIR $PFAM
 python ../pyCode/generating_bash_script.py $PFAM_DIR $PFAM
-#sbatch ${PFAM_DIR}/${PFAM}_cor_submit.sh 
-
+sbatch ${PFAM_DIR}/${PFAM}_cor_submit.sh
